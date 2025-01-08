d833bc49f595b455d53d77384458de206667a365
JohnleeHIT <525570844@qq.com>
2025-01-08 14:46:59 +0800

    change_file

---
+++ script.js
@@ -0,0 +1,7 @@
+function aat_dff_start(element) {
+    element.getElementsByClassName("two")[0].style.opacity = "1";
+}
+
+function aat_dff_stop(element) {
+    element.getElementsByClassName("two")[0].style.opacity = "0";
+}
